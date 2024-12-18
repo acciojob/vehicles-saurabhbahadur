27982d6ba85c018b84a1130a08a0379596a17d2b
Saurabh Bahadur <133571086+saurabhbahadur@users.noreply.github.com>
2024-12-18 15:38:16 +0530

    done

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,29 @@
+package com.driver;
+
+public class Boat implements WaterVehicle {
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
